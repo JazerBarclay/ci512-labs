2131369583d43209f8318740bc4beb63283a9fd0
Jazer <contact@jazer.co.uk>
2021-10-22 15:38:48 +0100

    Added graphs and kNN value error rates

--- lab3/knn.py
+++ lab3/knn.py
@@ -1,11 +1,18 @@
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import ShuffleSplit
+from sklearn.metrics import accuracy_score,precision_score
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import cross_val_predict
+
+import numpy as np
+np.random.seed(95)
+
 
 # Read the iris data from the comma separated file with the pandas library
-data = pd.read_csv("iris.data")
+data = pd.read_csv("iris.data", skiprows=0)
 
 print("----------------- CSV DATA ------------------")
 
@@ -16,14 +23,20 @@ with pd.option_context('display.max_rows', None, 'display.max_columns', None):  
 print("--------------- END OF DATA ----------------")
 print()
 
-# Number of neighbors used
+# Number of neighbors used to classify a test datapoint
 clf = KNeighborsClassifier(n_neighbors=3)
 
-# X uses all rows (:) and the first 4 columns as features (0-3)
+# X uses all rows (:) and the first 4 columns as attribute data (0-3)
 X = data.values[:, 0:3]
 
-# Y uses all rows (:) and the final column as the label (4) 
+# Y uses all rows (:) and the final column as the label data (4) 
 Y = data.values[:,4]
+
+h = .02  # step size in the mesh
+
+# Create color maps
+cmap_bold = ['darkorange', 'c', 'darkblue']
+
 
 # X_train is the training dataset without labels
 # X_test is the test set without labels
@@ -32,10 +45,17 @@ Y = data.values[:,4]
 # Set the test set size to a percentage (.2 = 20%) of the full dataset
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
 
+# Feature scaling
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
 # Add the test dataset to the k-nearest neighbors classifier
 clf = clf.fit(X_train, Y_train)
 
-# Stores teh prediction using the test set
+# Stores the prediction using the test set
 Y_prediction = clf.predict(X_test)
 
 # Print the prediction made compared to the actual results ('O' => Correct , 'X' => Incorrect)
@@ -50,8 +70,32 @@ print()
 # Print the accuracy of the prediction compared to the actual results
 print("Train/test accuracy:",accuracy_score(Y_test,Y_prediction))
 
+for vals in [[0,3], [1,4]]:
 
-from sklearn.model_selection import ShuffleSplit
+    X = data.values[:, vals[0]:vals[1]]
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                            np.arange(y_min, y_max, h))
+    # Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a plot
+    plt.figure(figsize=(8, 7))
+
+    # Plot also the training points
+    sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=Y,
+                    palette=cmap_bold, alpha=1.0, edgecolor="black")
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title("3-Class classification of length vs width")
+    plt.xlabel(("sepal" if vals[0] == 0 else "petal") + " length (cm)")
+    plt.ylabel(("sepal" if vals[0] == 0 else "petal") + " width (cm)")
+
+
 
 # Set shuffle_split params
 cv = ShuffleSplit(n_splits=5, test_size=0.2)
@@ -69,3 +113,32 @@ print("Cross fold validation accuracy scores:",scores)
 
 # Print the mean value for all tests
 print("Cross fold validation accuracy mean:",scores.mean())
+
+
+precision = precision_score(Y_test, Y_prediction, average="macro")
+print("Macro Precission Score: ", end='')
+print(precision)
+
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(Y_test, Y_prediction))
+print(classification_report(Y_test, Y_prediction))
+
+error = []
+
+# Calculating error for K values between 1 and 40
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, Y_train)
+    pred_i = knn.predict(X_test)
+    error.append(np.mean(pred_i != Y_test))
+
+plt.figure(figsize=(12, 6))
+plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o',
+         markerfacecolor='blue', markersize=10)
+plt.title('Error Rate K Value')
+plt.xlabel('K Value')
+plt.ylabel('Mean Error')
+
+# Display all stats in their own windows
+plt.show()
